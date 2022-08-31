cb0584da3b41dbd6dde47f1740dfdbbe38677102
Funke Faith Olasupo <fizzyflexy397@gmail.com>
2022-08-31 11:59:06 +0100

    fix:Handling Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,6 +47,13 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'success' => 0,
+                'data'   =>  'Resource Not Found'
+            ], 404);
         });
     }
 }
